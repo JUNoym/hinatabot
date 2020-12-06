1974a6033d7e0e434c9857ebb6da65f5506a431b
JUNoym <junnyaooyama@gmail.com>
2020-12-07 04:58:26 +0900

    editForm

--- src/Components/Forms/Form.jsx
+++ src/Components/Forms/Form.jsx
@@ -46,7 +46,7 @@ export default class Form extends React.Component {
                 + '【問い合わせ内容】\n' + description
         };
 
-        const url = "https://hooks.slack.com/services/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"             //urlを載せると通知が来なくなるので載せてません
+        const url = "*******************"  //urlを載せると通知が来なくなるので載せてません
 
         fetch(url, {
             method: "POST",
